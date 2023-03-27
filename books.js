7dc170685cd2c669b6fea5e7bbbc2fc59002b0fc
Larry Villegas <larry_villegas@hotmail.com>
2023-03-27 16:25:28 -0400

    First-steps

---
+++ books.js
@@ -0,0 +1,22 @@
+let myBooks = [
+  {
+    author: 'Larry Villegas',
+    Title: 'The magician',
+  },
+  {
+    author: 'Test 1',
+    Title: 'Book 1',
+  },  
+  {
+    author: 'Test 2',
+    Title: 'Book 2',
+  },  
+  {
+    author: 'Test 3',
+    Title: 'Book 3',
+  },  
+  {
+    author: 'Test 4',
+    Title: 'Book 4',
+  },
+];
