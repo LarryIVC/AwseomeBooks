5c6a5e5d67eb62707d23abf9071d381297498588
Larry Villegas <larry_villegas@hotmail.com>
2023-03-28 00:15:37 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,31 @@
-let arrBook = [
-  {
-    title: "Absalom",
-    author: "WILLIAM FAULKNER"
-  },
-  {
-    title: "TIME TO KILL",
-    author: "JOHN GRISHAM"
-  },
-  {
-    title: "THE HOUSE OF MIRTH",
-    author: "EDITH WHARTON"
-  },
-  {
-    title: "EAST OF EDEN",
-    author: "JOHN STEINBECK"
-  },
-];
+// let arrBook = [
+//   {
+//     title: "Absalom",
+//     author: "WILLIAM FAULKNER"
+//   },
+//   {
+//     title: "TIME TO KILL",
+//     author: "JOHN GRISHAM"
+//   },
+//   {
+//     title: "THE HOUSE OF MIRTH",
+//     author: "EDITH WHARTON"
+//   },
+//   {
+//     title: "EAST OF EDEN",
+//     author: "JOHN STEINBECK"
+//   },
+// ];
 
 const ListBooks = document.getElementById('book-container');
 // const btnAdd = document.getElementById('add');
 const formAdd = document.getElementById('frmAdd');
 
-
-
 // Display list of books 
 function showBooks() {
 
   let listHtml = '';
-
+  const arrBook = getBooks();
   arrBook.forEach((objBook, index) => {
     listHtml += `<div>
     <p>Title: ${objBook.title}</p>
@@ -44,6 +42,7 @@ function showBooks() {
   btnRemove.forEach((btnRemoveClick, index) => {
   btnRemoveClick.addEventListener('click', () => {
     arrBook.splice(index,1);  
+    saveBooks(arrBook); //save to LocalStorage
     showBooks();  
   });  
 });
@@ -55,16 +54,25 @@ showBooks();
 formAdd.addEventListener('submit', (e) => {
 
   e.preventDefault();
+  const arrBook= getBooks();
   const newBook = {
     title: formAdd.title.value,
     author: formAdd.author.value
   };
   arrBook.push(newBook);
+  saveBooks(arrBook); //save to LocalStorage
   showBooks();
   formAdd.title.value = '';
   formAdd.author.value = '';
 });
 
+function getBooks() {
+  const arrBook = JSON.parse(localStorage.getItem('arrBook')) || []; // Si no hay datos en el localStorage, devolvemos un array vacío
+  return arrBook;
+}
 
+function saveBooks(arrBook) {
+  localStorage.setItem('arrBook', JSON.stringify(arrBook));
+}
 
 
