21a799e1c341fb21cf8cc9bb02ed3f6393cbc26c
Naan Mohammed <naanahmed24@gmail.com>
2023-03-28 23:35:14 +0300

    Add classes to test.js and add loadbooks functionality

--- test.js
+++ test.js
@@ -1,51 +1,33 @@
-const ListBooks = document.getElementById('book-container');
-const formAdd = document.getElementById('formAdd');
-
-function getBooks() {
-  const arrBook = JSON.parse(localStorage.getItem('arrBook')) || [];
-  return arrBook;
-}
-
-function saveBooks(arrBook) {
-  localStorage.setItem('arrBook', JSON.stringify(arrBook));
-}
-
-// Display list of books
-function showBooks() {
-  let listHtml = '';
-  const arrBook = getBooks();
-  arrBook.forEach((objBook) => {
-    listHtml += `<div>
-    <p>Title: ${objBook.title}</p>
-    <p>Author: ${objBook.author}</p>
-    <button class = "btnRemove">Remove</button>
-    <hr></div>`;
-  });
-
-  ListBooks.innerHTML = listHtml;
-  //  remove book
-  const btnRemove = document.querySelectorAll('.btnRemove');
-  btnRemove.forEach((btnRemoveClick, index) => {
-    btnRemoveClick.addEventListener('click', () => {
-      arrBook.splice(index, 1);
-      saveBooks(arrBook); // save to LocalStorage
-      showBooks();
-    });
-  });
-}
-
-showBooks();
-// add book function
-formAdd.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const arrBook = getBooks();
-  const newBook = {
-    title: formAdd.title.value,
-    author: formAdd.author.value,
-  };
-  arrBook.push(newBook);
-  saveBooks(arrBook); // save to LocalStorage
-  showBooks();
-  formAdd.title.value = '';
-  formAdd.author.value = '';
-});
+class Book {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
+  
+  class BookList {
+    constructor(bookListElement, addBookFormElement, titleInputElement, authorInputElement) {
+      this.bookListElement = bookListElement;
+      this.addBookFormElement = addBookFormElement;
+      this.titleInputElement = titleInputElement;
+      this.authorInputElement = authorInputElement;
+      this.books = [];
+      this.loadBooks();
+      this.updateBookList();
+      this.addBookFormElement.addEventListener("submit", this.add.bind(this));
+    }
+  
+    loadBooks() {
+      const storedBooks = JSON.parse(localStorage.getItem("books"));
+      if (storedBooks) {
+        this.books = storedBooks.map(book => new Book(book.title, book.author));
+      }
+    }
+  }
+  
+  const bookListElement = document.getElementById("book-list");
+  const addBookFormElement = document.getElementById("add-book-form");
+  const titleInputElement = document.getElementById("title-input");
+  const authorInputElement = document.getElementById("author-input");
+  
+  const bookList = new BookList(bookListElement, addBookFormElement, titleInputElement, authorInputElement);
