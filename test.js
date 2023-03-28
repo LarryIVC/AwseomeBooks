396bf9c2e269e0278d8d0ecaf2adfebdfb466515
Naan Mohammed <naanahmed24@gmail.com>
2023-03-29 00:51:46 +0300

    Fix linter errors

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-classes-per-file */
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -6,11 +8,11 @@ class Book {
 }
 
 class BookList {
-  constructor(bookListElement, addBookFormElement, titleInputElement, authorInputElement) {
+  constructor(bookListElement, addBookFormElement, titleInputElement, authorInput) {
     this.bookListElement = bookListElement;
     this.addBookFormElement = addBookFormElement;
     this.titleInputElement = titleInputElement;
-    this.authorInputElement = authorInputElement;
+    this.authorInput = authorInput;
     this.books = [];
     this.loadBooks();
     this.updateBookList();
@@ -20,29 +22,29 @@ class BookList {
   loadBooks() {
     const storedBooks = JSON.parse(localStorage.getItem('books'));
     if (storedBooks) {
-      this.books = storedBooks.map(book => new Book(book.title, book.author));
+      this.books = storedBooks.map(((book) => new Book(book.title, book.author)));
     }
-  };
+  }
 
   add(event) {
     event.preventDefault();
     const title = this.titleInputElement.value;
-    const author = this.authorInputElement.value;
+    const author = this.authorInput.value;
     const book = new Book(title, author);
     this.books.push(book);
     this.titleInputElement.value = '';
-    this.authorInputElement.value = '';
+    this.authorInput.value = '';
     this.updateBookList();
-  };
+  }
 
   removeBook(title) {
-    this.books = this.books.filter(book => book.title !== title);
+    this.books = this.books.filter(((book) => book.title !== title));
     this.updateBookList();
-  };
+  }
 
   updateBookList() {
     this.bookListElement.innerHTML = '';
-    this.books.forEach(book => {
+    this.books.forEach(((book) => {
       const listItem = document.createElement('li');
       listItem.innerHTML = `<strong>${book.title}</strong> by ${book.author}`;
       const removeButton = document.createElement('button');
@@ -55,14 +57,17 @@ class BookList {
       listItem.appendChild(removeButton);
       listItem.appendChild(breakline);
       this.bookListElement.appendChild(listItem);
-    });
+    }));
     localStorage.setItem('books', JSON.stringify(this.books));
-  };
-};
+  }
+}
 
 const bookListElement = document.getElementById('book-list');
 const addBookFormElement = document.getElementById('add-book-form');
 const titleInputElement = document.getElementById('title-input');
-const authorInputElement = document.getElementById('author-input');
+const authorInput = document.getElementById('author-input');
 
-const bookList = new BookList(bookListElement, addBookFormElement, titleInputElement, authorInputElement);
+const bookList = new BookList(bookListElement, addBookFormElement, titleInputElement, authorInput);
+
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-classes-per-file */
